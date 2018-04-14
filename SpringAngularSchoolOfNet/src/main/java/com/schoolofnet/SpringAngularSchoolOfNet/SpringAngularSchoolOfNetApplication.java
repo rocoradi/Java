259aea18fe76aa353c388947a051145a3f41ed05
Rodrigo Coradi <wcrodrigo@hotmail.com>
2018-04-13 23:53:51 -0300

    Novo Projeto

---
+++ SpringAngularSchoolOfNet/src/main/java/com/schoolofnet/SpringAngularSchoolOfNet/SpringAngularSchoolOfNetApplication.java
@@ -0,0 +1,12 @@
+package com.schoolofnet.SpringAngularSchoolOfNet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAngularSchoolOfNetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAngularSchoolOfNetApplication.class, args);
+	}
+}
