034b5993590fb537af3e4ecc92b8fde54c7c76c1
Rodrigo Coradi <wcrodrigo@hotmail.com>
2018-04-14 17:52:37 -0300

    curso spring/angular schoolf

--- SpringAngularSchoolOfNet/src/main/java/com/schoolofnet/SpringAngularSchoolOfNet/SpringAngularSchoolOfNetApplication.java
+++ SpringAngularSchoolOfNet/src/main/java/com/schoolofnet/SpringAngularSchoolOfNet/SpringAngularSchoolOfNetApplication.java
@@ -2,11 +2,30 @@ package com.schoolofnet.SpringAngularSchoolOfNet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @SpringBootApplication
 public class SpringAngularSchoolOfNetApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringAngularSchoolOfNetApplication.class, args);
+	}
+	
+	@Bean
+	CorsConfigurationSource configurationSource() {
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
+		
+		corsConfiguration.addAllowedHeader("*");
+		corsConfiguration.addAllowedMethod("*");
+		corsConfiguration.addAllowedOrigin("*");
+//		corsConfiguration.addExposedHeader("");
+		
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", corsConfiguration);
+		
+		return source;
 	}
 }
