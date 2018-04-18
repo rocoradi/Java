9704e3272d72e7d91695bc79749f6b829c58a9cb
Rodrigo Coradi <wcrodrigo@hotmail.com>
2018-04-17 23:39:13 -0300

    ok

--- algamoney-api/src/main/java/com/algamoney/api/controller/CategoriaController.java
+++ algamoney-api/src/main/java/com/algamoney/api/controller/CategoriaController.java
@@ -1,11 +1,19 @@
 package com.algamoney.api.controller;
 
+import java.net.URI;
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.algamoney.api.model.Categoria;
 import com.algamoney.api.repository.CategoriaRepository;
@@ -20,6 +28,24 @@ public class CategoriaController {
 	@GetMapping
 	public List<Categoria> listar(){
 		return categoriaRepository.findAll();
+	}
+	
+	@PostMapping
+	public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria, HttpServletResponse response) {
+		Categoria categoriaSalva = categoriaRepository.save(categoria);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+				  .buildAndExpand(categoriaSalva.getCodigo())
+				  .toUri();
+		
+		return ResponseEntity.created(uri).body(categoriaSalva);
+	}
+	
+	@GetMapping("/{codigo}")
+	public ResponseEntity<Categoria> buscaPeloCodigo(@PathVariable Long codigo) {
+		Categoria categoria = categoriaRepository.findOne(codigo);
+		
+		return categoria != null ? ResponseEntity.ok(categoria) : ResponseEntity.notFound().build();
 	}
 	
 }
