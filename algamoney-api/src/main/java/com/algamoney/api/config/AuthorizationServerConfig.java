5076b0ced46a3f04053b513e30dec5f26dfe65c1
Rodrigo Coradi <wcrodrigo@hotmail.com>
2018-05-18 00:19:53 -0300

    seguranca oauth

--- algamoney-api/src/main/java/com/algamoney/api/config/AuthorizationServerConfig.java
+++ algamoney-api/src/main/java/com/algamoney/api/config/AuthorizationServerConfig.java
@@ -25,14 +25,16 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 			   .withClient("angular")
 			   .secret("@ngul@r0")
 			   .scopes("read","write")
-			   .authorizedGrantTypes("password")
-			   .accessTokenValiditySeconds(1800);
+			   .authorizedGrantTypes("password", "refresh_token")
+			   .accessTokenValiditySeconds(20)
+			   .refreshTokenValiditySeconds(3600 * 24);
 	}
 	
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
 		endpoints.tokenStore(tokenStore())
 				 .accessTokenConverter(accessTokenConverter())
+				 .reuseRefreshTokens(false)
 			     .authenticationManager(authenticationManager);
 	}
 	
