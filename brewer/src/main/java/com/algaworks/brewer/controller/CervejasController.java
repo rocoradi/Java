11738abc1cb6e3fadf71743e752b851d1cc27dc1
Rodrigo Coradi <wcrodrigo@hotmail.com>
2018-07-07 12:05:58 -0300

    projeto spring experts

--- brewer/src/main/java/com/algaworks/brewer/controller/CervejasController.java
+++ brewer/src/main/java/com/algaworks/brewer/controller/CervejasController.java
@@ -1,8 +1,38 @@
 package com.algaworks.brewer.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.algaworks.brewer.model.Cerveja;
 
 @Controller
+@RequestMapping("/cervejas")
 public class CervejasController {
 
+	@GetMapping("/novo")
+	public String novo() {
+		return "cerveja/CadastroCerveja";
+	}
+	
+	@PostMapping("/novo")
+	public String cadastrar(@Valid Cerveja cerveja, BindingResult result, Model model, RedirectAttributes attributes) {
+		
+		if (result.hasErrors()) {
+			model.addAttribute("mensagem", "Erro no formulário");
+			return "cerveja/CadastroCerveja";
+		}
+		
+		// Salvar no banco de dados...
+		attributes.addFlashAttribute("mensagem", "Cerveja salva com sucesso!");
+		System.out.println(">>> sku: " + cerveja.getSku());
+		return "redirect:/cervejas/novo";
+	}
+	
 }
